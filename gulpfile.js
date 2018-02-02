6dacac70f6f253e88dd950fe12bb8cb18560360c
Conrad Jamir <cjamir@grupogallegos.com>
2018-02-02 14:53:16 -0800

    added node environment variables

--- gulpfile.js
+++ gulpfile.js
@@ -11,40 +11,52 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
+    sassDir = 'components/sass/',
+    scriptsDir = 'components/scripts/',
+    env,
+    outputDir,
+    sassStyle,
     paths;
  
+env = process.env.NODE_ENV || 'development';
+// in terminal change to production: NODE_ENV=production gulp
+
+if(env === 'development'){
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
 // paths object of source files
 paths = {
   styles: {
-    src: 'components/sass',
-    sass: 'components/sass/*.scss',
-    styleSass: 'components/sass/style.scss',
-    devDest: 'builds/development/css',
-    proDest: 'builds/production/css'
+    dir: sassDir,
+    sass: sassDir + '*.scss',
+    styleSass: sassDir + 'style.scss',
+    dest: outputDir + 'css'
   },
   scripts: {
-    src: 'components/scripts',
+    dir: scriptsDir,
     coffee: 'components/coffee/*.coffee',
-    js: 'components/scripts/*js',
+    js: scriptsDir + '*js',
     // could also be an array to re-arrange order of concatenation
     jsOrder: [
-      'components/scripts/rclick.js',
-      'components/scripts/pixgrid.js',
-      'components/scripts/taline.js',
-      'components/scripts/template.js',
+      scriptsDir + 'rclick.js',
+      scriptsDir + 'pixgrid.js',
+      scriptsDir + 'taline.js',
+      scriptsDir + 'template.js',
     ],
-    devDest: 'builds/development/js',
-    proDest: 'builds/production/js'
+    dest: outputDir + 'js'
   },
   images: {
-    devDest: 'builds/development/images',
-    proDest: 'builds/development/images'
+    dest: outputDir + 'images'
   },
   builds: {
-    devDest: 'builds/development',
-    proDest: 'builds/production',
-    html: 'builds/development/*.html',
-    json: 'builds/development/js/*.json'
+    dest: outputDir,
+    html: outputDir + '*.html',
+    json: outputDir + '*.json'
   }
 };
 
@@ -58,7 +70,7 @@ gulp.task('coffee', function(){
   gulp.src(paths.scripts.coffee) //src('location of file to process'), or src([]) array of files.
       .pipe(coffee({ bare: true }) //pipe() sends source to coffee() w/object parameters bare:true so it formats w/no safety wrapper
       .on('error', gutil.log))//outputs a log error in the terminal
-      .pipe(gulp.dest(paths.scripts.src)); //write out processed script into directory location
+      .pipe(gulp.dest(paths.scripts.dir)); //write out processed script into scripts directory
 });
 
 // process js files with gulp-concat and browserify into one js file
@@ -66,7 +78,7 @@ gulp.task('js', function(){ // you can process other tasks by adding array: gulp
   gulp.src(paths.scripts.js) //src('location of file to process'), or src([]) array of files.
       .pipe(concat('script.js')) //pipe() sends js sources to concat() library to process into script.js which is the name of js file in html.
       .pipe(browserify()) //pipe() takes in browserify() method that adds jquery and mustache libraries to the script.js
-      .pipe(gulp.dest(paths.scripts.devDest)) //pipe() sends script.js to destination folder.
+      .pipe(gulp.dest(paths.scripts.dest)) //pipe() sends script.js to destination folder.
       .pipe(connect.reload()); //pipe() triggers the connect reload() method to reload page when task is run.
 });
 
@@ -74,14 +86,14 @@ gulp.task('js', function(){ // you can process other tasks by adding array: gulp
 gulp.task('compass', function(){
   gulp.src(paths.styles.styleSass) //src('location of style.scss file to process')
       .pipe(compass({
-        sass: paths.styles.src, //src of all .scss files
-        image: paths.images.devDest, //src of images
-        css: paths.styles.devDest, //destination to send processed style.css (bug in program, if not specified, a copy of css folder is written in the root folder)
-        style: 'expanded', //other option is compressed, see sass api for different styles
+        sass: paths.styles.dir, //src of all .scss files
+        image: paths.images.dest, //src of images
+        css: paths.styles.dest, //destination to send processed style.css (bug in program, if not specified, a copy of css folder is written in the root folder)
+        style: sassStyle, //other option is compressed, see sass api for different styles
         comments: true //adds comments/line numbers in css where the style originated from
       })) //pipe() sends style.scss to compass() library to process into style.css
       .on('error', gutil.log) //outputs a log error in the terminal
-      .pipe(gulp.dest(paths.styles.devDest)) //pipe() sends style.css to destination folder.
+      .pipe(gulp.dest(paths.styles.dest)) //pipe() sends style.css to destination folder.
       .pipe(connect.reload()); //pipe() triggers the connect reload() method to reload page when task is run.
 });
 
@@ -109,7 +121,7 @@ gulp.task('watch', function(){
 // task to launch a webserver and do a live reload
 gulp.task('connect', function(){
   connect.server({
-    root: paths.builds.devDest,
+    root: paths.builds.dest,
     livereload: true
   });
 });
