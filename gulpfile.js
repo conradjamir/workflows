ffd8e55a385b64344d0655c53b214f1ade968988
Conrad Jamir <cjamir@grupogallegos.com>
2018-02-01 22:06:52 -0800

    added data.json, gulp-connect server and live-reload

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     paths;
  
 // paths object of source files
@@ -25,6 +26,7 @@ paths = {
     src: 'components/scripts',
     coffee: 'components/coffee/*.coffee',
     js: 'components/scripts/*js',
+    // could also be an array to re-arrange order of concatenation
     jsOrder: [
       'components/scripts/rclick.js',
       'components/scripts/pixgrid.js',
@@ -37,6 +39,10 @@ paths = {
   images: {
     devDest: 'builds/development/images',
     proDest: 'builds/development/images'
+  },
+  builds: {
+    devDest: 'builds/development',
+    proDest: 'builds/production'
   }
 };
 
@@ -48,10 +54,8 @@ gulp.task('log', function(){
 // process coffee scripts into javascript using gulp-coffee
 gulp.task('coffee', function(){
   gulp.src(paths.scripts.coffee) //src('location of file to process'), or src([]) array of files.
-      .pipe(coffee({
-          bare: true
-        }).on('error', gutil.log) //outputs a log error in the terminal
-      ) //pipe() sends source to coffee() library with object parameters bare:true so it formats w/no safety wrapper
+      .pipe(coffee({ bare: true }) //pipe() sends source to coffee() w/object parameters bare:true so it formats w/no safety wrapper
+      .on('error', gutil.log))//outputs a log error in the terminal
       .pipe(gulp.dest(paths.scripts.src)); //write out processed script into directory location
 });
 
@@ -60,7 +64,8 @@ gulp.task('js', function(){ // you can process other tasks by adding array: gulp
   gulp.src(paths.scripts.js) //src('location of file to process'), or src([]) array of files.
       .pipe(concat('script.js')) //pipe() sends js sources to concat() library to process into script.js which is the name of js file in html.
       .pipe(browserify()) //pipe() takes in browserify() method that adds jquery and mustache libraries to the script.js
-      .pipe(gulp.dest(paths.scripts.devDest)); //pipe() sends script.js to destination folder.
+      .pipe(gulp.dest(paths.scripts.devDest)) //pipe() sends script.js to destination folder.
+      .pipe(connect.reload()); //pipe() triggers the connect reload() method to reload page when task is run.
 });
 
 // process SASS/Compas files with gulp-compass
@@ -74,16 +79,25 @@ gulp.task('compass', function(){
         comments: true //adds comments/line numbers in css where the style originated from
       })) //pipe() sends style.scss to compass() library to process into style.css
       .on('error', gutil.log) //outputs a log error in the terminal
-      .pipe(gulp.dest(paths.styles.devDest)); //pipe() sends style.css to destination folder.
+      .pipe(gulp.dest(paths.styles.devDest)) //pipe() sends style.css to destination folder.
+      .pipe(connect.reload()); //pipe() triggers the connect reload() method to reload page when task is run.
 });
 
 // task to watch for changes gulp.watch(source, task)
 gulp.task('watch', function(){
   gulp.watch(paths.scripts.coffee, ['coffee']);
-  gulp.watch(paths.scripts.js, ['js']);
+  gulp.watch(paths.scripts.js, ['js']); //option to add connect.reload()
   gulp.watch(paths.styles.sass, ['compass']);
 });
 
+// task to launch a webserver and do a live reload
+gulp.task('connect', function(){
+  connect.server({
+    root: paths.builds.devDest,
+    livereload: true
+  });
+});
+
 // if task name is 'default' just type: gulp in terminal
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']); 
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']); 
 
